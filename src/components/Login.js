35e1df72dd86f2f375b0a1d9f5b1b29a50d4f923
cashcoder88 <cashglobe@gmail.com>
2019-05-23 13:55:04 -0600

    styling login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,61 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
-import axios from 'axios';
+import styled from 'styled-components';
+
+const LoginWrapper = styled.div`
+    width: 350px;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 10px 5px 5px grey;
+    margin: 10px;
+    padding: 10px;
+    color: #303030;
+    
+    
+`
+
+const FormWrapper = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-top: 1px solid grey;
+    padding-top: 10px;
+    width: 300px;
+    align-items: center;
+    
+    
+    
+`
+
+const BoxDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`
+
+const Input = styled.input`
+    width: 250px;
+    height: 25px;
+    border-radius: 5px;
+    margin: 10px 0 30px 0;
+
+`
+
+const Button = styled.button`
+    width: 250px;
+    height: 30px;
+    background: #BB1333;
+    color: white;
+    border-radius: 5px;
+    font-size: 14px;
+
+`
+
+const WelcomeDiv = styled.div`
+
+`
 
 
 class Login extends React.Component {
@@ -13,49 +68,39 @@ class Login extends React.Component {
     }
 
 
-    // submitLogin = (event) => {
-    //     event.preventDefault();
-    //     axios
-    //     .post('https://lambdafit.herokuapp.com/auth/login', this.state)
-    //     .then(res => {
-    //         console.log('result', res)
-    //         localStorage.setItem('token', res.data.token)
-    //         this.props.history.push('/')
-    //         window.location.reload(); 
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-    // }
-
 
 
     render() {
         return (
-            <div className='login-wrapper'>
+            <LoginWrapper>
                 <div>
-                    <h2>Login With Username and Password</h2>
+                    <h2>Hello</h2>
+                    <h4>Please Login With Your Username and Password</h4>
                 </div>
-                <div className='input'>
-                    <form>
-                        <input 
-                        type="text" 
-                        placeholder="Username"
-                        name="username"
-                        value={this.props.username}
-                        onChange={this.props.handleInput}
-                        />
-                        <input 
-                        type="password"
-                        placeholder='Password'
-                        name="password"
-                        value={this.props.password}
-                        onChange={this.props.handleInput}
-                        />
-                        <button onClick={this.props.submitLogin}>Login</button>
-                    </form>
+                <div>
+                    <FormWrapper>
+                            <BoxDiv>
+                                <label>Login</label>
+                                <Input 
+                                type="text" 
+                                placeholder="Username"
+                                name="username"
+                                value={this.props.username}
+                                onChange={this.props.handleInput}
+                                />
+                                <label>Password</label>
+                                <Input 
+                                type="password"
+                                placeholder='Password'
+                                name="password"
+                                value={this.props.password}
+                                onChange={this.props.handleInput}
+                                />
+                            </BoxDiv>
+                        <Button onClick={this.props.submitLogin}>Login</Button>
+                    </FormWrapper>
                 </div>
-            </div>
+            </LoginWrapper>
         )
     }
 }
